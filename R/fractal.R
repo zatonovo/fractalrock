9a163bcad333bf414737a95ab5fefffc9a6b8d2a
Brian Lee Yung Rowe <rowe@zatonovo.com>
2015-02-02 19:56:24 +0800

    Fix up rintraday

--- R/fractal.R
+++ R/fractal.R
@@ -215,3 +215,66 @@ fractal.random <- function(seed, patterns, count=NULL, epochs=NULL,
 }
 
 
+
+# Code to make data
+#seed.1 <- matrix(c(13460,10, 13507,20, 13566,18, 13600,24), ncol=2, byrow=TRUE)
+#seed.2 <- matrix(c(13460,24, 13488,28, 13600,27), ncol=2, byrow=TRUE)
+#seed.3 <- matrix(c(13460,48, 13521,45, 13600,52), ncol=2, byrow=TRUE)
+#seed.4 <- matrix(c(13460,18, 13517,20, 13528,19, 13600,22), ncol=2, byrow=TRUE)
+#seed.5 <- matrix(c(13460,52, 13499,67, 13520,65, 13600,62), ncol=2, byrow=TRUE)
+#seed.6 <- matrix(c(13460,98, 13474,89, 13588,99, 13600,95), ncol=2, byrow=TRUE)
+#seed.7 <- matrix(c(13460,85, 13556,81, 13600,83), ncol=2, byrow=TRUE)
+#seed.8 <- matrix(c(13460,22, 13522,21, 13534,23, 13549,25, 13600,24), ncol=2, byrow=TRUE)
+#seed.9 <- matrix(c(13460,65, 13512,68, 13538,65, 13600,69), ncol=2, byrow=TRUE)
+#sampleInitiators <- list(seed.1=seed.1, seed.2=seed.2, 
+#  seed.3=seed.3, seed.4=seed.4, seed.5=seed.5, seed.6=seed.6,
+#  seed.7=seed.7, seed.8=seed.8, seed.9=seed.9)
+
+#pattern.1 <- matrix(c(0, 0.2, 0.4, 0.1, 0.7,-0.3, 1,0.2), ncol=2, byrow=TRUE)
+##pattern.2 <- matrix(c(0,-0.1, 0.3, 0.2, 0.6,-0.2, 1,0.4), ncol=2, byrow=TRUE)
+#pattern.3 <- matrix(c(0,-0.3, 0.4,-0.2, 0.7, 0.3, 1,0.2), ncol=2, byrow=TRUE)
+#pattern.4 <- matrix(c(0, 0.7, 0.2, 0.4, 0.5, 0.4, 1,0.1), ncol=2, byrow=TRUE)
+#pattern.5 <- matrix(c(0, 0.1, 0.2, 0.3, 0.4, 0.3, 1,0.1), ncol=2, byrow=TRUE)
+#pattern.6 <- matrix(c(0,-0.2, 0.1, 0.2, 0.5, 0.2, 1,0.1), ncol=2, byrow=TRUE)
+#pattern.7 <- matrix(c(0, 0.3, 0.4,-0.1, 0.6, 0.4, 1,0.2), ncol=2, byrow=TRUE)
+#pattern.8 <- matrix(c(0, 0.0, 0.3, 0.0, 0.7,-0.2, 1,0.1), ncol=2, byrow=TRUE)
+#pattern.9 <- matrix(c(0, 0.4, 0.2, 0.2, 0.6, 0.1, 1,0.3), ncol=2, byrow=TRUE)
+#sampleGenerators <- list(pattern.1=pattern.1, pattern.2=pattern.2,
+#  pattern.3=pattern.3, pattern.4=pattern.4, pattern.5=pattern.5,
+#  pattern.6=pattern.6, pattern.7=pattern.7, pattern.8=pattern.8,
+#  pattern.9=pattern.9)
+#save(sampleInitiators, sampleGenerators, file='fractalrock/data/generators.RData')
+
+#ps <- fractal(seed, pats, 4)
+#ps <- fractal(seed, pats, 4, only=4)
+#plotReturns(ps)
+
+# Simulate order sign
+# (Incomplete)
+#microInitiators <- list(
+#  seed.1=matrix(c(600.0, 1, 602.4,-1, 603.4,-1,  610.0,-1), ncol=2,byrow=TRUE),
+#  seed.2=matrix(c(600.0,-1, 601.2,-1, 602.8, 1,  610.0, 1), ncol=2,byrow=TRUE),
+#  seed.3=matrix(c(600.0, 1, 603.1, 1, 604.2,-1,  610.0, 1), ncol=2,byrow=TRUE)
+#)
+#microGenerators <- list(
+##  pattern.1=matrix(c(0, 1, 0.22, 1, 0.61,-1,  1,-1), ncol=2,byrow=TRUE),
+##  pattern.2=matrix(c(0,-1, 0.40, 1, 0.63, 1,  1, 1), ncol=2,byrow=TRUE),
+##  pattern.3=matrix(c(0,-1, 0.55,-1, 0.83,-1,  1, 1), ncol=2,byrow=TRUE),
+##  pattern.4=matrix(c(0,-1, 0.55,-1, 0.83,-1,  1, 1), ncol=2,byrow=TRUE)
+#)
+#ps <- getPortfolioPrices('IBM',10, seeds=microInitiators, patterns=microGenerators, date.fun=as.POSIXct)
+
+# Unused
+#.fracret <- function(assets=10, epochs=3)
+#{
+#  fn <- function(x)
+#  {
+#    series <- fractal(seed, pats, epochs=epochs)
+#    series <- Delt(series)
+#    series <- series[! is.na(series) & ! is.infinite(series)]
+#    series <- series - mean(series)
+#  }
+#  rets <- do.call(cbind, lapply(1:assets, fn))
+#  names(rets) <- 1:assets
+#  rets
+#}
--- R/framework.R
+++ R/framework.R
@@ -115,89 +115,83 @@ plot_returns <- function(series, ...)
   invisible(series)
 }
 
-# Code to make data
-#seed.1 <- matrix(c(13460,10, 13507,20, 13566,18, 13600,24), ncol=2, byrow=TRUE)
-#seed.2 <- matrix(c(13460,24, 13488,28, 13600,27), ncol=2, byrow=TRUE)
-#seed.3 <- matrix(c(13460,48, 13521,45, 13600,52), ncol=2, byrow=TRUE)
-#seed.4 <- matrix(c(13460,18, 13517,20, 13528,19, 13600,22), ncol=2, byrow=TRUE)
-#seed.5 <- matrix(c(13460,52, 13499,67, 13520,65, 13600,62), ncol=2, byrow=TRUE)
-#seed.6 <- matrix(c(13460,98, 13474,89, 13588,99, 13600,95), ncol=2, byrow=TRUE)
-#seed.7 <- matrix(c(13460,85, 13556,81, 13600,83), ncol=2, byrow=TRUE)
-#seed.8 <- matrix(c(13460,22, 13522,21, 13534,23, 13549,25, 13600,24), ncol=2, byrow=TRUE)
-#seed.9 <- matrix(c(13460,65, 13512,68, 13538,65, 13600,69), ncol=2, byrow=TRUE)
-#sampleInitiators <- list(seed.1=seed.1, seed.2=seed.2, 
-#  seed.3=seed.3, seed.4=seed.4, seed.5=seed.5, seed.6=seed.6,
-#  seed.7=seed.7, seed.8=seed.8, seed.9=seed.9)
-
-#pattern.1 <- matrix(c(0, 0.2, 0.4, 0.1, 0.7,-0.3, 1,0.2), ncol=2, byrow=TRUE)
-##pattern.2 <- matrix(c(0,-0.1, 0.3, 0.2, 0.6,-0.2, 1,0.4), ncol=2, byrow=TRUE)
-#pattern.3 <- matrix(c(0,-0.3, 0.4,-0.2, 0.7, 0.3, 1,0.2), ncol=2, byrow=TRUE)
-#pattern.4 <- matrix(c(0, 0.7, 0.2, 0.4, 0.5, 0.4, 1,0.1), ncol=2, byrow=TRUE)
-#pattern.5 <- matrix(c(0, 0.1, 0.2, 0.3, 0.4, 0.3, 1,0.1), ncol=2, byrow=TRUE)
-#pattern.6 <- matrix(c(0,-0.2, 0.1, 0.2, 0.5, 0.2, 1,0.1), ncol=2, byrow=TRUE)
-#pattern.7 <- matrix(c(0, 0.3, 0.4,-0.1, 0.6, 0.4, 1,0.2), ncol=2, byrow=TRUE)
-#pattern.8 <- matrix(c(0, 0.0, 0.3, 0.0, 0.7,-0.2, 1,0.1), ncol=2, byrow=TRUE)
-#pattern.9 <- matrix(c(0, 0.4, 0.2, 0.2, 0.6, 0.1, 1,0.3), ncol=2, byrow=TRUE)
-#sampleGenerators <- list(pattern.1=pattern.1, pattern.2=pattern.2,
-#  pattern.3=pattern.3, pattern.4=pattern.4, pattern.5=pattern.5,
-#  pattern.6=pattern.6, pattern.7=pattern.7, pattern.8=pattern.8,
-#  pattern.9=pattern.9)
-#save(sampleInitiators, sampleGenerators, file='fractalrock/data/generators.RData')
-
-#ps <- fractal(seed, pats, 4)
-#ps <- fractal(seed, pats, 4, only=4)
-#plotReturns(ps)
-
-# Simulate order sign
-# (Incomplete)
-#microInitiators <- list(
-#  seed.1=matrix(c(600.0, 1, 602.4,-1, 603.4,-1,  610.0,-1), ncol=2,byrow=TRUE),
-#  seed.2=matrix(c(600.0,-1, 601.2,-1, 602.8, 1,  610.0, 1), ncol=2,byrow=TRUE),
-#  seed.3=matrix(c(600.0, 1, 603.1, 1, 604.2,-1,  610.0, 1), ncol=2,byrow=TRUE)
-#)
-#microGenerators <- list(
-##  pattern.1=matrix(c(0, 1, 0.22, 1, 0.61,-1,  1,-1), ncol=2,byrow=TRUE),
-##  pattern.2=matrix(c(0,-1, 0.40, 1, 0.63, 1,  1, 1), ncol=2,byrow=TRUE),
-##  pattern.3=matrix(c(0,-1, 0.55,-1, 0.83,-1,  1, 1), ncol=2,byrow=TRUE),
-##  pattern.4=matrix(c(0,-1, 0.55,-1, 0.83,-1,  1, 1), ncol=2,byrow=TRUE)
-#)
-#ps <- getPortfolioPrices('IBM',10, seeds=microInitiators, patterns=microGenerators, date.fun=as.POSIXct)
-
-# Unused
-#.fracret <- function(assets=10, epochs=3)
-#{
-#  fn <- function(x)
-#  {
-#    series <- fractal(seed, pats, epochs=epochs)
-#    series <- Delt(series)
-#    series <- series[! is.na(series) & ! is.infinite(series)]
-#    series <- series - mean(series)
-#  }
-#  rets <- do.call(cbind, lapply(1:assets, fn))
-#  names(rets) <- 1:assets
-#  rets
-#}
 
 
-rintraday(symbol, obs, process, end=Sys.Date(), start=NULL, calendar=holidayNYSE, period=DAILY, ohlc=FALSE, volume=FALSE) %as% {
-  dates <- trading_dates(end, start, obs, calendar, period)
+#' Create correlated intraday prices
+#'
+#' @examples
+#' myou <- function(x) ou(x, 120, 3/24)
+#' seed <- rintraday(myou, obs=60,period=1)
+#' cmat <- matrix(c(1,0,0, .8,1,0, .6,.4,1), ncol=3)
+#' y <- rintraday(seed, cmat)
+rintraday(series, rho) %::% xts : matrix : xts
+rintraday(series, rho) %as% {
+  if (all(rho[lower.tri(rho)] == 0))
+    rho[lower.tri(rho)] <- rho[upper.tri(rho)]
+
+  cd <- chol(nearPD(rho)$mat)
+  u <- mean(series)
+  s <- sd(series)
+  num.cols <- ncol(rho) - 1
+  r <- cbind((series - u)/s, 
+    matrix(rnorm(nrow(series) * num.cols), ncol=num.cols))
+  rc <- r %*% cd
+  out <- as.xts(u + rc * s, order.by=index(series))
+  colnames(out) <- colnames(rho)
+  out
+}
+
+#' myou <- function(x) ou(x, 120, 3/24)
+#' seed <- rintraday(myou, obs=60,period=1)
+#' cmat <- matrix(c(1,0,0, .8,1,0, .6,.4,1), ncol=3)
+#' y <- rintraday(seed, cmat, ohlc=1, volume=100)
+rintraday(series, rho, ohlc, volume) %::% xts : matrix : . : . : list
+rintraday(series, rho, ohlc=FALSE, volume=FALSE) %as% {
+  prices <- rintraday(series, rho)
+  colnames(prices) <- rep('close',ncol(prices))
+  out <- lapply(1:ncol(prices), function(x) prices[,x])
+
+  if (ohlc) out <- lapply(out, function(x) .add_ohlc(x, ohlc))
+  if (volume) out <- lapply(out, function(x) .add_volume(x, volume))
+  out
+}
+
+#' Simulate intraday prices
+#'
+#' @examples
+#' myou <- function(x) ou(x, 40, 3/24, 45, .03/1440)
+#' x <- rintraday(myou, obs=60,period=1)
+rintraday(process, end=Sys.Date(), ohlc=FALSE, volume=FALSE, ...) %as% {
+  dates <- trading_dates(end=end, ...)
   n <- length(dates)
   prices <- as.xts(process(n), order.by=dates)
-  rownames(prices) <- format(dates)
+  #rownames(prices) <- format(dates)
   colnames(prices) <- 'close'
 
-  if (ohlc) {
-    open <- as.numeric(prices$close[1]) + rnorm(1) # This is totally a kludge
-    prices$open <- c(open,prices$close[1:(n-1)])
-    prices$low <- pmin(prices$open,prices$close) - abs(rnorm(n, sd=ohlc))
-    prices$high <- pmax(prices$open,prices$close) + abs(rnorm(n, sd=ohlc))
-  }
-  if (volume) {
-    prices$volume <- as.integer(abs(rnorm(n, mean=volume, sd=volume/4)))
-  }
+  if (ohlc) prices <- .add_ohlc(prices, ohlc)
+  if (volume) prices <- .add_volume(prices, volume)
   prices
 }
 
+
+.add_ohlc <- function(series, sd) {
+  n <- nrow(series)
+  open <- as.numeric(series$close[1]) + rnorm(1) # This is totally a kludge
+  series$open <- c(open,series$close[1:(n-1)])
+  series$low <- pmin(series$open,series$close) - abs(rnorm(n, sd=sd))
+  series$high <- pmax(series$open,series$close) + abs(rnorm(n, sd=sd))
+  series
+}
+
+.add_volume <- function(series, mean, sd=mean/4) {
+  n <- nrow(series)
+  series$volume <- as.integer(abs(rnorm(n, mean=mean, sd=sd)))
+  series
+}
+
+
+#' Simulate prices
+#'
 #' @examples
 #' rprices(c("A","B","C"), 10, function(n) rnorm(n))
 #' rprices(c("A","B","C"), 10, function(n) gbm(n, runif(1,10,200)))
