fe6e3db39fa047f1c21efc988cde574ef9a6d170
Brian Lee Yung Rowe <rowe@zatonovo.com>
2015-02-03 10:44:05 +0800

    Fix bugs

--- R/ou.R
+++ R/ou.R
@@ -12,10 +12,13 @@ ou.process <- function(theta, mu=0, sigma=1, initial=mu,
   xts(series + noise, order.by=dates)
 }
 
+#' @param lambda The shock, in annualized terms
+#' @param mu The mean-reverting value
+#' @param sigma The annualized standard deviation of the noise term
 ou(n, s=0.04, lambda=3, mu=0.01, sigma=0.03) %as% {
   ts <- 1:n
   series <- s * exp(-lambda/252 * ts) + mu * (1 - exp(-lambda/252 * ts))
-  noise <- rnorm(n,sqrt(sigma * (1 - exp(-lambda/252)) / (2 * lambda)))
+  noise <- rnorm(n, sd=sqrt(sigma^2 * (1-exp(-2*lambda/252*ts)) / (2*lambda)))
   series + noise
 }
 
