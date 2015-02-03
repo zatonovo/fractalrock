e43d7d22404cdadd92815c41ee6687d6d764996a
Brian Lee Yung Rowe <rowe@zatonovo.com>
2015-02-03 13:26:28 +0800

    Tune OU

--- R/ou.R
+++ R/ou.R
@@ -18,7 +18,7 @@ ou.process <- function(theta, mu=0, sigma=1, initial=mu,
 ou(n, s=0.04, lambda=3, mu=0.01, sigma=0.03) %as% {
   ts <- 1:n
   series <- s * exp(-lambda/252 * ts) + mu * (1 - exp(-lambda/252 * ts))
-  noise <- rnorm(n, sd=sqrt(sigma^2 * (1-exp(-2*lambda/252*ts)) / (2*lambda)))
+  noise <- rnorm(n, sd=sqrt(sigma^2 * (1-exp(-lambda/252)) / (2*lambda)))
   series + noise
 }
 
