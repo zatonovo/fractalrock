a4650d8b28b146e3c29f64befd123557e6f1f25a
iwccsbfb <zengye.wang@hotmail.com>
2016-09-26 14:02:51 -0400

    Update test-example.R

--- tests/testit/test-example.R
+++ tests/testit/test-example.R
@@ -1,2 +1,17 @@
 # See https://github.com/yihui/testit
 assert('Tautology is true', TRUE, TRUE)
+
+date <- '2016-01-01'
+res <- trading_dates(date, 0)
+expect_that(is.null(res), is_true())
+res <- trading_dates(date, 1)
+expect_that(res, equals(as.Date('2016-01-04')))
+res <- trading_dates(date, 2)
+expect_that(res, equals(as.Date(c('2016-01-04', '2016-01-05'))))
+
+date <- '2016-01-04'
+res <- trading_dates(date, -1)
+expect_that(res, equals(as.Date('2016-01-04')))
+res <- trading_dates(date, -2)
+expect_that(res, equals(as.Date(c('2015-12-31', '2016-01-04'))))
+
